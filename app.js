5cc0e19b74d2d45e2859ec647e2d9240c39355b7
Sahil Gupta <sahil.gupta26.sg@gmail.com>
2019-11-21 00:16:13 +0530

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
 //seedDB();
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb+srv://admin:sahilsahil@cluster0-wnd4p.mongodb.net/test?retryWrites=true&w=majority");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
