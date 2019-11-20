077c100116987f4a7fc942aa4459ba03153dd918
Sahil Gupta <sahil.gupta26.sg@gmail.com>
2019-11-21 00:30:58 +0530

    change port

--- app.js
+++ app.js
@@ -51,4 +51,4 @@ passport.deserializeUser(User.deserializeUser());
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
